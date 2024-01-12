0788ae8b0d90b16d37e07232712295bed9566d9d
Mustafa Kemal ARDA <mkemalarda@gmail.com>
2024-01-12 17:52:07 +0300

    revision1 -> image,stack view added

--- TasarimCalismasi/ViewController.swift
+++ TasarimCalismasi/ViewController.swift
@@ -11,7 +11,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.navigationItem.title = "Pizza"     // Ekranın üst kısmına pizza kelimesini yazdırdık
+        
+        
+        let appearance = UINavigationBarAppearance()
+        
+        appearance.backgroundColor = UIColor(named: "AnaRenk")                              // asset kısmında tanımladığımız rengi aktardık
+        appearance.titleTextAttributes = [.foregroundColor:UIColor(named: "YaziRenk1")!,.font: UIFont(name: "Pacifico-Regular", size: 22)!]    // pizza yazısını "yazirenk1" rengine çevirdik
+        
+        navigationController?.navigationBar.barStyle = .black       // Ekranın üst kısımdaki simgeleri beyaza çevirir
+        
+        
+        navigationController?.navigationBar.standardAppearance = appearance         // Ekranın üst kısımlarına tanımladığımız rengi aktardık
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+
+        
+        
+        
     }
 
 
